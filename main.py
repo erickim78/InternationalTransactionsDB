c3573b49dbb197dabab5d17bbb1fde64d2446bba
ericdkim78@gmail.com <ericdkim78@gmail.com>
2019-11-04 00:17:32 -0800

    Finished Initial Commit

--- main.py
+++ main.py
@@ -7,11 +7,11 @@ def create_table( season ):
     cursor.execute(f'CREATE TABLE IF NOT EXISTS {season}( TransferDate TEXT, PlayerName TEXT, PrevTeam TEXT, CurrentTeam TEXT)')
 
 def insert_data( season, date, name, prevt, currt):
-    cursor.execute(f'INSERT INTO {season} VALUES( {date}, {name}, {prevt}, {currt})')
+    cursor.execute(f'INSERT INTO {season} VALUES( \'{date}\', \'{name}\', \'{prevt}\', \'{currt}\')')
     db.commit()
 
-
-
+#create_table( 'Season20102011' )
+#insert_data( 'Season20102011', '2019-01-01', 'Alex Caruso', 'Clippers', 'Lakers')
 
 cursor.close()
 db.close()
